66602fb206e6cce0a554718353aa52593487d7b5
Patrick Lee <me@patricklee.nyc>
2016-06-24 21:22:58 -0400

    added 404 page and testing it

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   var redirect_url;
   redirect_url = $('#redirect_url').html();
-  $(location).attr("href", redirect_url);
+  //$(location).attr("href", redirect_url);
+
+  pathname = window.location.pathname;
+  console.log(pathname);
+
 });
