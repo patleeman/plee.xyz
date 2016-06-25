144e53015c8585bf08181825076fbbecbe418b89
Patrick Lee <me@patricklee.nyc>
2016-06-24 21:56:10 -0400

    URL lookup fn added

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,22 @@ $(document).ready(function(){
   redirect_url = $('#redirect_url').html();
 
   // Get pathname
-  pathname = window.location.pathname;
+  pathname = window.location.pathname
   path_split = pathname.split("/")
   path = path_split[path_split.length - 1]
 
   // Lookup pathname in data
+  path = path_lookup(path)
 
-
-  console.log(URL_LOOKUP);
+  console.log(path);
   //$(location).attr("href", redirect_url);
 });
+
+function path_lookup(key) {
+  url = URL_LOOKUP[key];
+  if (url){
+    return url;
+  } else {
+    return null;
+  }
+}
