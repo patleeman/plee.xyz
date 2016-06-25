0d038a9f032431615240eb7b106e89feac8b8740
Patrick Lee <me@patricklee.nyc>
2016-06-24 21:50:40 -0400

    added url lookup file

---
+++ data.js
@@ -0,0 +1,4 @@
+var URL_LOOKUP = {
+  "abc": "http://www.example.com",
+  "def": "http://www.patricklee.nyc"
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
   var redirect_url;
   redirect_url = $('#redirect_url').html();
-  //$(location).attr("href", redirect_url);
 
+  // Get pathname
   pathname = window.location.pathname;
   path_split = pathname.split("/")
   path = path_split[path_split.length - 1]
-  console.log(path);
 
+  // Lookup pathname in data
+
+
+  console.log(URL_LOOKUP);
+  //$(location).attr("href", redirect_url);
 });
