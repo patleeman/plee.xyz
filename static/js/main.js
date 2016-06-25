b091438518c71d12ea8b34bc91cde17b47916a3e
Patrick Lee <me@patricklee.nyc>
2016-06-24 22:08:47 -0400

    working prototype?

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,23 @@
 
 
 $(document).ready(function(){
-  var redirect_url;
-  redirect_url = $('#redirect_url').html();
-
   // Get pathname
   pathname = window.location.pathname
   path_split = pathname.split("/")
   path = path_split[path_split.length - 1]
 
-  // Lookup pathname in data
-  path = path_lookup(path)
+  destination_url = lookup_path(path)
 
-  console.log(path);
-  //$(location).attr("href", redirect_url);
+  if (destination_url && destination_url != undefined){
+    $(location).attr("href", destination_url);
+  } else {
+    $(location).attr("href", "notfound.html");
+  }
+
 });
 
-function path_lookup(key) {
+// Lookup path in data file
+function lookup_path(key) {
   url = URL_LOOKUP[key];
-  if (url){
-    return url;
-  } else {
-    console.log(url);
-    return null;
-  }
+  return url;
 }
